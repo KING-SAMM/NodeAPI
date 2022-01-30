f5179a5974eb40c4c378f6b5c54202382902a2fe
King Samm <kcsamm11@gmail.com>
2022-01-30 18:33:15 +0100

    Modified main.js

--- main.js
+++ main.js
@@ -4,16 +4,16 @@
  */
 
 // Dependencies
-const http = require('http');
-const https = require('https');
-const url = require('url');
-const StringDecoder = require('string_decoder').StringDecoder;
-const config = require('./config');
-const fs = require('fs');
+var http = require('http');
+var https = require('https');
+var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
+var config = require('./config');
+var fs = require('fs');
 
 
  // Instantiate the HTTP server
-const httpServer = http.createServer(function(req,res){
+var httpServer = http.createServer(function(req,res){
   unifiedServer(req,res);
 });
 
@@ -23,11 +23,11 @@ httpServer.listen(config.httpPort,function(){
 });
 
 // Instantiate the HTTPS server
-const httpsServerOptions = {
+var httpsServerOptions = {
   'key': fs.readFileSync('./https/key.pem'),
   'cert': fs.readFileSync('./https/cert.pem')
 };
-const httpsServer = https.createServer(httpsServerOptions,function(req,res){
+var httpsServer = https.createServer(httpsServerOptions,function(req,res){
   unifiedServer(req,res);
 });
 
@@ -37,27 +37,27 @@ httpsServer.listen(config.httpsPort,function(){
 });
 
 // All the server logic for both the http and https server
-const unifiedServer = function(req,res){
+var unifiedServer = function(req,res){
 
   // Parse the url
-  const parsedUrl = url.parse(req.url, true);
+  var parsedUrl = url.parse(req.url, true);
 
   // Get the path
-  const path = parsedUrl.pathname;
-  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
   // Get the query string as an object
-  const queryStringObject = parsedUrl.query;
+  var queryStringObject = parsedUrl.query;
 
   // Get the HTTP method
-  const method = req.method.toLowerCase();
+  var method = req.method.toLowerCase();
 
   //Get the headers as an object
-  const headers = req.headers;
+  var headers = req.headers;
 
   // Get the payload,if any
-  const decoder = new StringDecoder('utf-8');
-  const buffer = '';
+  var decoder = new StringDecoder('utf-8');
+  var buffer = '';
   req.on('data', function(data) {
       buffer += decoder.write(data);
   });
@@ -65,10 +65,10 @@ const unifiedServer = function(req,res){
       buffer += decoder.end();
 
       // Check the router for a matching path for a handler. If one is not found, use the notFound handler instead.
-      const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+      var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
 
       // Construct the data object to send to the handler
-      const data = {
+      var data = {
         'trimmedPath' : trimmedPath,
         'queryStringObject' : queryStringObject,
         'method' : method,
@@ -86,7 +86,7 @@ const unifiedServer = function(req,res){
         payload = typeof(payload) == 'object'? payload : {};
 
         // Convert the payload to a string
-        const payloadString = JSON.stringify(payload);
+        var payloadString = JSON.stringify(payload);
 
         // Return the response
         res.setHeader('Content-Type', 'application/json');
@@ -100,7 +100,7 @@ const unifiedServer = function(req,res){
 };
 
 // Define all the handlers
-const handlers = {};
+var handlers = {};
 
 // Ping handler
 handlers.ping = function(data,callback){
@@ -113,6 +113,6 @@ handlers.notFound = function(data,callback){
 };
 
 // Define the request router
-const router = {
+var router = {
   'ping' : handlers.ping
 };
